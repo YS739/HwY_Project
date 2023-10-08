bb2a3c8367a414a49d0b00b2890ae70d2ec8b791
YS739 <suk921@gmail.com>
2023-10-08 15:53:27 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+const Home = () => {
+  return (
+    <main className={styles.main}>
+      <div>Language Learning app with GPT</div>
+    </main>
+  );
+};
+export default Home;
