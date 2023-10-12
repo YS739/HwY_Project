2e3b88417e089267bb54ac07f8e7f751aad1178d
YS739 <suk921@gmail.com>
2023-10-12 16:28:25 +0900

    chore: eslint 및 prettier 설정, MUI 설치

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import styles from "./page.module.css";
+import styles from './page.module.css';
+import { Button } from '@mui/material';
 
+// TODO:: 전체 코드 작성 후 component 분리
 const Home = () => {
   return (
     <main className={styles.main}>
       <div>Language Learning app with GPT</div>
+      {/* mui custom test - sx prop 사용 */}
+      <Button
+        sx={{
+          color: 'red',
+          backgroundColor: 'skyblue',
+        }}
+        variant="text"
+      >
+        Text
+      </Button>
+      <Button variant="contained">Contained</Button>
     </main>
   );
 };
